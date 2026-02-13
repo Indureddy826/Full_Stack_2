86830f50477de0a18e9919ac0b58c043beb78c9e
Indureddy826 <indureddy826@gmail.com>
2026-02-13 10:42:40 +0530

    Initial commit

---
+++ Full_Stack_2-main/EXP-1/EXP1.1/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Full Stack Project Running 🚀</h1>
+      <p>Welcome Indureddy!</p>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
